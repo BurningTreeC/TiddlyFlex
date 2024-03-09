c378252166e673db0ca1dea070c043028639219a
BurningTreeC <huber.simon@protonmail.com>
2024-03-09 06:31:52 +0100

    add tiddlyflex-draggable which doesn't refresh when enable attribute changes

---
+++ TiddlyFlex/files/modules/utils/dom/tiddlyflex-utils.js
@@ -0,0 +1,163 @@
+/*\
+title: $:/plugins/BTC/TiddlyFlex/modules/utils/dom/tiddlyflex-utils.js
+type: application/javascript
+module-type: utils
+
+Browser data transfer utilities, used with the clipboard and drag and drop
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Options:
+
+domNode: dom node to make draggable
+selector: CSS selector to identify element within domNode to be used as drag handle (optional)
+dragImageType: "pill", "blank" or "dom" (the default)
+dragTiddlerFn: optional function to retrieve the title of tiddler to drag
+dragFilterFn: optional function to retreive the filter defining a list of tiddlers to drag
+widget: widget to use as the context for the filter
+*/
+exports.makeTiddlyFlexDraggable = function(options) {
+	var dragImageType = options.dragImageType || "dom",
+		dragImage,
+		domNode = options.domNode,
+		removeEventHandler = options.remove;
+	// Make the dom node draggable (not necessary for anchor tags)
+	if(!removeEventHandler && !options.selector && ((domNode.tagName || "").toLowerCase() !== "a")) {
+		domNode.setAttribute("draggable","true");
+	} else if(removeEventHandler) {
+		domNode.removeAttribute("draggable");
+	}
+	var dragStartHandlerFunction = function(event) {
+		if(event.dataTransfer === undefined) {
+			return false;
+		}
+		// Collect the tiddlers being dragged
+		var dragTiddler = options.dragTiddlerFn && options.dragTiddlerFn(),
+			dragFilter = options.dragFilterFn && options.dragFilterFn(),
+			titles = dragTiddler ? [dragTiddler] : [],
+			startActions = options.startActions,
+			variables,
+			domNodeRect;
+		if(dragFilter) {
+			titles.push.apply(titles,options.widget.wiki.filterTiddlers(dragFilter,options.widget));
+		}
+		var titleString = $tw.utils.stringifyList(titles);
+		// Check that we've something to drag
+		if(titles.length > 0 && (options.selector && $tw.utils.domMatchesSelector(event.target,options.selector) || event.target === domNode)) {
+			// Mark the drag in progress
+			$tw.dragInProgress = domNode;
+			// Set the dragging class on the element being dragged
+			$tw.utils.addClass(domNode,"tc-dragging");
+			// Invoke drag-start actions if given
+			if(startActions !== undefined) {
+				// Collect our variables
+				variables = $tw.utils.collectDOMVariables(domNode,null,event);
+				variables.modifier = $tw.keyboardManager.getEventModifierKeyDescriptor(event);
+				variables["actionTiddler"] = titleString;
+				options.widget.invokeActionString(startActions,options.widget,event,variables);
+			}
+			// Create the drag image elements
+			dragImage = options.widget.document.createElement("div");
+			dragImage.className = "tc-tiddler-dragger";
+			var inner = options.widget.document.createElement("div");
+			inner.className = "tc-tiddler-dragger-inner";
+			inner.appendChild(options.widget.document.createTextNode(
+				titles.length === 1 ? 
+					titles[0] :
+					titles.length + " tiddlers"
+			));
+			dragImage.appendChild(inner);
+			options.widget.document.body.appendChild(dragImage);
+			// Set the data transfer properties
+			var dataTransfer = event.dataTransfer;
+			// Set up the image
+			dataTransfer.effectAllowed = "all";
+			if(dataTransfer.setDragImage) {
+				if(dragImageType === "pill") {
+					dataTransfer.setDragImage(dragImage.firstChild,-16,-16);
+				} else if(dragImageType === "blank") {
+					dragImage.removeChild(dragImage.firstChild);
+					dataTransfer.setDragImage(dragImage,0,0);
+				} else {
+					var r = domNode.getBoundingClientRect();
+					dataTransfer.setDragImage(domNode,event.clientX-r.left,event.clientY-r.top);
+				}
+			}
+			// Set up the data transfer
+			if(dataTransfer.clearData) {
+				dataTransfer.clearData();
+			}
+			var jsonData = [];
+			if(titles.length > 1) {
+				titles.forEach(function(title) {
+					jsonData.push(options.widget.wiki.getTiddlerAsJson(title));
+				});
+				jsonData = "[" + jsonData.join(",") + "]";
+			} else {
+				jsonData = options.widget.wiki.getTiddlerAsJson(titles[0]);
+			}
+			// IE doesn't like these content types
+			if(!$tw.browser.isIE) {
+				dataTransfer.setData("text/vnd.tiddler",jsonData);
+				dataTransfer.setData("text/plain",titleString);
+				dataTransfer.setData("text/x-moz-url","data:text/vnd.tiddler," + encodeURIComponent(jsonData));
+			}
+			// If browser is Chrome-like and has a touch-input device do NOT .setData
+			if(!($tw.browser.isMobileChrome)) {
+				dataTransfer.setData("URL","data:text/vnd.tiddler," + encodeURIComponent(jsonData));
+			}
+			dataTransfer.setData("Text",titleString);
+			event.stopPropagation();
+		}
+		return false;
+	};
+
+	var dragEndHandlerFunction = function(event) {
+		if((options.selector && $tw.utils.domMatchesSelector(event.target,options.selector)) || event.target === domNode) {
+			// Collect the tiddlers being dragged
+			var dragTiddler = options.dragTiddlerFn && options.dragTiddlerFn(),
+				dragFilter = options.dragFilterFn && options.dragFilterFn(),
+				titles = dragTiddler ? [dragTiddler] : [],
+				endActions = options.endActions,
+				variables;
+			if(dragFilter) {
+				titles.push.apply(titles,options.widget.wiki.filterTiddlers(dragFilter,options.widget));
+			}
+			var titleString = $tw.utils.stringifyList(titles);
+			$tw.dragInProgress = null;
+			// Invoke drag-end actions if given
+			if(endActions !== undefined) {
+				variables = $tw.utils.collectDOMVariables(domNode,null,event);
+				variables.modifier = $tw.keyboardManager.getEventModifierKeyDescriptor(event);
+				variables["actionTiddler"] = titleString;
+				options.widget.invokeActionString(endActions,options.widget,event,variables);
+			}
+			// Remove the dragging class on the element being dragged
+			$tw.utils.removeClass(domNode,"tc-dragging");
+			// Delete the drag image element
+			if(dragImage) {
+				dragImage.parentNode.removeChild(dragImage);
+				dragImage = null;
+			}
+		}
+		return false;
+	};
+
+	if(removeEventHandler === false) {
+		// Add event handlers
+		options.widget.dragStartListenerReference = dragStartHandlerFunction;
+		options.widget.dragEndListenerReference = dragEndHandlerFunction;
+		$tw.utils.addEventListeners(domNode,[
+			{name: "dragstart", handlerFunction: dragStartHandlerFunction},
+			{name: "dragend", handlerFunction: dragEndHandlerFunction}
+		]);
+	}
+};
+
+})();
---
+++ TiddlyFlex/files/modules/widgets/tiddlyflex-draggable.js
@@ -0,0 +1,153 @@
+/*\
+title: $:/core/modules/widgets/draggable.js
+type: application/javascript
+module-type: widget
+
+Draggable widget
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+
+var DraggableWidget = function(parseTreeNode,options) {
+	this.initialise(parseTreeNode,options);
+};
+
+/*
+Inherit from the base widget class
+*/
+DraggableWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+DraggableWidget.prototype.render = function(parent,nextSibling) {
+	var self = this,
+		tag,
+		domNode,
+		classes = [];
+	// Save the parent dom node
+	this.parentDomNode = parent;
+	// Compute our attributes
+	this.computeAttributes();
+	// Execute our logic
+	this.execute();
+	// Sanitise the specified tag
+	tag = this.draggableTag;
+	if($tw.config.htmlUnsafeElements.indexOf(tag) !== -1) {
+		tag = "div";
+	}
+	// Create our element
+	domNode = this.document.createElement(tag);
+	// Assign classes
+	if(this.draggableClasses) {
+		classes.push(this.draggableClasses);
+	}
+	if(!this.dragHandleSelector && this.dragEnable) {
+		classes.push("tc-draggable");
+	}
+	domNode.setAttribute("class",classes.join(" "));
+	// Assign data- attributes and style. attributes
+	this.assignAttributes(domNode,{
+		sourcePrefix: "data-",
+		destPrefix: "data-"
+	});
+	// Insert the node into the DOM and render any children
+	parent.insertBefore(domNode,nextSibling);
+	this.renderChildren(domNode,null);
+	this.makeDraggable(domNode);
+	this.domNodes.push(domNode);
+};
+
+DraggableWidget.prototype.makeDraggable = function(domNode) {
+	var self = this;
+	// Add event handlers
+	if(this.dragEnable) {
+		$tw.utils.makeTiddlyFlexDraggable({
+			domNode: domNode,
+			remove: false,
+			dragTiddlerFn: function() {return self.getAttribute("tiddler");},
+			dragFilterFn: function() {return self.getAttribute("filter");},
+			startActions: self.startActions,
+			endActions: self.endActions,
+			dragImageType: self.dragImageType,
+			widget: this,
+			selector: self.dragHandleSelector
+		});
+	} else if(this.dragStartListenerReference && this.dragEndListenerReference) {
+		domNode.removeEventListener("dragstart",this.dragStartListenerReference,false);
+		domNode.removeEventListener("dragend",this.dragEndListenerReference,false);
+	}
+};
+
+/*
+Compute the internal state of the widget
+*/
+DraggableWidget.prototype.execute = function() {
+	// Pick up our attributes
+	this.draggableTag = this.getAttribute("tag","div");
+	this.draggableClasses = this.getAttribute("class");
+	this.startActions = this.getAttribute("startactions");
+	this.endActions = this.getAttribute("endactions");
+	this.dragImageType = this.getAttribute("dragimagetype");
+	this.dragHandleSelector = this.getAttribute("selector");
+	this.dragEnable = this.getAttribute("enable","yes") === "yes";
+	// Make the child widgets
+	this.makeChildWidgets();
+};
+
+
+DraggableWidget.prototype.assignDomNodeClasses = function() {
+	var domNodeClasses = this.domNodes[0].className.split(" "),
+		oldClasses = this.draggableClasses.split(" ");
+	this.draggableClasses = this.getAttribute("class");
+	//Remove classes assigned from the old value of class attribute
+	$tw.utils.each(oldClasses,function(oldClass){
+		var i = domNodeClasses.indexOf(oldClass);
+		if(i !== -1) {
+			domNodeClasses.splice(i,1);
+		}
+	});
+	//Add new classes from updated class attribute.
+	$tw.utils.pushTop(domNodeClasses,this.draggableClasses);
+	this.domNodes[0].setAttribute("class",domNodeClasses.join(" "))
+}
+
+/*
+Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+*/
+DraggableWidget.prototype.refresh = function(changedTiddlers) {
+	var changedAttributes = this.computeAttributes();
+	if(changedAttributes.tag || changedAttributes.selector || changedAttributes.dragimagetype || changedAttributes.startactions || changedAttributes.endactions) {
+		this.refreshSelf();
+		return true;
+	} else {
+		if(changedAttributes["class"]) {
+			this.assignDomNodeClasses();
+		}
+		if(changedAttributes["enable"]) {
+			this.dragEnable = this.getAttribute("enable","yes") === "yes";
+			this.makeDraggable(this.domNodes[0]);
+			if(!this.dragHandleSelector && this.dragEnable && !this.domNodes[0].classList.contains("tc-draggable")) {
+				this.domNodes[0].classList.add("tc-draggable");
+			} else if(!this.dragEnable && this.domNodes[0].classList.contains("tc-draggable")) {
+				this.domNodes[0].classList.remove("tc-draggable");
+			}
+		}
+		this.assignAttributes(this.domNodes[0],{
+			changedAttributes: changedAttributes,
+			sourcePrefix: "data-",
+			destPrefix: "data-"
+		});
+	}
+	return this.refreshChildren(changedTiddlers);
+};
+
+exports["tiddlyflex-draggable"] = DraggableWidget;
+
+})();
